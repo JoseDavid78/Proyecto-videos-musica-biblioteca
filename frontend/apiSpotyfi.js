3804fb0f5d97c54ba5593ba37468b22380e272c9
JoseDavid78 <merlopozo@gmail.com>
2024-03-21 14:01:45 +0100

    dia 2

---
+++ frontend/apiSpotyfi.js
@@ -0,0 +1,26 @@
+const boton = document.querySelector("button");
+const resultadoInput = document.querySelector("input");
+const apiURL = "https://spotify23.p.rapidapi.com/search/?q=";
+
+boton.addEventListener("click", () => {
+    const nombreCancion = resultadoInput.value.trim();
+
+    if (nombreCancion.length === 0) {
+        alert("El campo está vacío");
+        return;
+    }
+
+    fetch(apiURL + encodeURIComponent(nombreCancion) + "&type=multi&offset=0&limit=10&numberOfTopResults=5", {
+        method: "GET",
+        headers: {
+            "x-rapidapi-host": "spotify23.p.rapidapi.com",
+            "x-rapidapi-key": "<TU_CLAVE_API_RAPIDAPI>",
+            "useQueryString": true
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => alert(error));
+});
